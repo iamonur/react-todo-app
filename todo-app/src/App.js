5f915721a7d5821c0d085d1492ca3ff7ad882223
Onur Tekik <otekik@gmail.com>
2024-07-24 13:14:00 +0300

    First commit for first video :)

---
+++ todo-app/src/App.js
@@ -0,0 +1,54 @@
+
+import React, {useState} from 'react';
+import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="App">
+      <h1>To-Do List</h1>
+      <TodoForm addTodo={addTodo}/>
+      <TodoList todos={todos} removeTodo={removeTodo}/>
+    </div>
+  );
+}
+export default App;
+
+/*import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
